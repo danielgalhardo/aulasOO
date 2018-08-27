54100b4a4717c8f54c03a65275a35f40fb413e74
ice <ice@lab06m02.nrc.ice.ufjf.br>
2018-08-27 20:11:11 -0300

    Classe Cachorro

---
+++ AulaDia27/src/auladia27/Cachorro.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package auladia27;
+
+/**
+ *
+ * @author ice
+ */
+public class Cachorro {
+                     private  String nome;
+                     private  int idade;
+                     private  String raça;
+
+    public Cachorro(String nome, int idade, String raça) {
+        this.nome = nome;
+        this.idade = idade;
+        this.raça = raça;
+    }
+             
+                       
+}
